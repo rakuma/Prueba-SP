d24dee91bb24ecf7572c5414ccd0280e4ccee5ce
Daniel <rzakuma26@gmail.com>
2014-10-02 14:29:26 -0600

    agrego archivos
    Agrego todos los archivos iniciales a GitHub

---
+++ footer.php
@@ -0,0 +1,46 @@
+<div class="fbg">
+    <div class="fbg_resize">
+      <div class="col c1">
+        <h2>Acerca de Nosotros</h2>
+        <img src="images/white.jpg" width="66" height="66" alt="" />
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius.</p>
+        <p>llorem, eu posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum odio, ac blandit ante orci ut diam.</p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna.</p>
+      </div>
+      <div class="col c2">
+        <h2>Sistemas Paper</h2>
+        <p>Lorem ipsum dolor<br />
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum odio, ac blandit ante orci ut diam.</p>
+        <ul class="sb_menu">
+          <li><a href="#">consequat molestie</a></li>
+          <li><a href="#">sem justo</a></li>
+          <li><a href="#">semper</a></li>
+          <li><a href="#">magna sed purus</a></li>
+          <li><a href="#">tincidunt</a></li>
+        </ul>
+      </div>
+      <div class="col c3">
+        <h2>Image Gallery</h2>
+        <a href="#"><img src="images/pix1.jpg" width="66" height="66" alt="" class="ad" /></a> <a href="#"><img src="images/pix2.jpg" width="66" height="66" alt="" class="ad" /></a> <a href="#"><img src="images/pix3.jpg" width="66" height="66" alt="" class="ad" /></a> <a href="#"><img src="images/pix4.jpg" width="66" height="66" alt="" class="ad" /></a> <a href="#"><img src="images/pix5.jpg" width="66" height="66" alt="" class="ad" /></a> <a href="#"><img src="images/pix6.jpg" width="66" height="66" alt="" class="ad" /></a>
+        <h2>Contact Info</h2>
+        <p><strong>Phone:</strong> +1234567<br />
+          <strong>Address:</strong> 123 TemplateAccess Rd<br />
+          <strong>E-mail:</strong> <a href="#">info@example.com</a></p>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="footer_resize">
+      <p class="lf">&copy; Copyright MyWebSite. Designed by Blue <a href="http://www.bluewebtemplates.com/">Website Templates</a></p>
+      <ul class="fmenu">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="support.html">Support</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li><a href="about.html">About Us</a></li>
+        <li><a href="contact.html">Contacts</a></li>
+      </ul>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
---
+++ header.php
@@ -0,0 +1,20 @@
+<div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <h1><a href="home.php"><span>Paper</span> Depot<br />
+          <small></small></a></h1>
+      </div>
+      
+      <div class="clr"></div>
+      <div class="menu">
+        <ul>
+          <li class="active"><a href="home.php"><span>Inicio</span></a></li>
+          <li><a href="support.html"><span>Support</span></a></li>
+          <li><a href="about.html"><span>About Us</span></a></li>
+          <li><a href="blog.html"><span>Blog</span></a></li>
+          <li><a href="contact.html"><span>Contact Us</span></a></li>
+        </ul>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
